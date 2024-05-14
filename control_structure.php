5ba199bddd90aaa294125c8c22139465dccb0c03
Hendrawan5rb <hendrawan5rb@gmail.com>
2024-05-14 11:39:26 +0700

    Function

--- control_structure.php
+++ control_structure.php
@@ -1,81 +1,84 @@
-<?php
-
-/* Perulangan for terdiri dari 3 bagian.
-1. Inisialisasi (Menentukan variabel awal untuk pengulangan. Biasanya $i, index.)
-2. Kondisi Terminasi (Kondisi untuk memberhentikan perulangannya.)
-3. Increment/Decrement (Perulangan maju atau mundur.) */
-
-for ($i = 0; $i < 9; $i++) {
-    echo "Anpan! ";
-}
-
-$w = 0;
-while ($w < 7) {
-    echo "Anpan! ";
-    $w++;
-}
-
-$a = 0;
-do {
-    echo "Anpan! ";
-    $a++;
-} while ($a <= 4);
-
-echo "<br><br>";
-
-if (true == true) {
-    echo "If";
-}
-
-echo "<br>";
-
-if (true == true) {
-    echo "If Else True";
-} else {
-    echo "If Else False";
-}
-
-echo "<br>";
-
-$tes = -3;
-if ($tes < 10) {
-    echo "Kurang dari 10";
-} elseif ($tes == 10) {
-    echo "10";
-} else {
-    echo "Lebih dari 10";
-}
-
-echo "<br>";
-
-$tes2 = 4;
-switch ($tes2) {
-    case 1:
-        echo "1";
-        break;
-    case 2:
-        echo "2";
-        break;
-    case 3:
-        echo "3";
-        break;
-    default:
-        echo "IDK";
-        break;
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Control Structure</title>
 </head>
 
 <body>
+
+    <h2>Selanjutnya, <a href="function.php"><?= "Function" ?></h2></a>
+
+    <?php
+
+    /* Perulangan for terdiri dari 3 bagian.
+    1. Inisialisasi (Menentukan variabel awal untuk pengulangan. Biasanya $i, index.)
+    2. Kondisi Terminasi (Kondisi untuk memberhentikan perulangannya.)
+    3. Increment/Decrement (Perulangan maju atau mundur.) */
+
+    for ($i = 0; $i < 9; $i++) {
+        echo "Anpan! ";
+    }
+
+    $w = 0;
+    while ($w < 7) {
+        echo "Anpan! ";
+        $w++;
+    }
+
+    $a = 0;
+    do {
+        echo "Anpan! ";
+        $a++;
+    } while ($a <= 4);
+
+    echo "<br><br>";
+
+    if (true == true) {
+        echo "If";
+    }
+
+    echo "<br>";
+
+    if (true == true) {
+        echo "If Else True";
+    } else {
+        echo "If Else False";
+    }
+
+    echo "<br>";
+
+    $tes = -3;
+    if ($tes < 10) {
+        echo "Kurang dari 10";
+    } elseif ($tes == 10) {
+        echo "10";
+    } else {
+        echo "Lebih dari 10";
+    }
+
+    echo "<br>";
+
+    $tes2 = 4;
+    switch ($tes2) {
+        case 1:
+            echo "1";
+            break;
+        case 2:
+            echo "2";
+            break;
+        case 3:
+            echo "3";
+            break;
+        default:
+            echo "IDK";
+            break;
+    }
+
+    ?>
+
     <br><br>
     <table border="1">
         <?php for ($baris = 1; $baris <= 9; $baris++) : ?>
---
+++ function.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Function</title>
+</head>
+
+<body>
+
+    <h1><?= salam("Pagi", "Zee"); ?></h1>
+
+    <?php
+    // ---Ada Built-in Function PHP---
+    echo date("l, d-M-Y") . "<br>";
+    echo time() . "<br>";
+    echo date("l", time() + 60 * 60 * 24 * 100) . "<br>";
+    /* mktime(0,0,0,0,0,0)
+    jam, menit, detik, bulan, tanggal, tahun */
+    echo date("l", mktime(0, 0, 0, 5, 26, 2002)) . "<br>";
+    echo date("l", strtotime("5 may 2002")) . "<br>";
+
+    // ---Ada User Defined Function PHP---
+    function salam($waktu = "Datang", $nama = "Admin")
+    {
+        return "Selamat $waktu, $nama";
+    }
+    ?>
+
+</body>
+
+</html>
